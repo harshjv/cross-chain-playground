f1560a8d72f33d847a23671ffaee88b9fea648a0
Harsh Vakharia <harshjv@gmail.com>
2019-05-13 17:46:41 +0530

    yet another commit

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,6 @@
     <nav class="navbar navbar-expand-lg navbar-light">
       <div class="container">
         <router-link class="navbar-brand" to="/">Cross Chain Playground</router-link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
       </div>
     </nav>
     <router-view/>
