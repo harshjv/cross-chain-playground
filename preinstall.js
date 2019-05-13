a1a814c53a578d5ce1f9ce1b1af13c295de28ba0
Harsh Vakharia <harshjv@gmail.com>
2019-05-13 17:44:18 +0530

    yet another commit

--- preinstall.js
+++ preinstall.js
@@ -1,19 +1,11 @@
-if (process.env.NETLIFY) {
-  // Because netlify can't build usb package :/
-  // and we don't need it for the web build
+// Because netlify can't build usb package :/
+// and we don't need it for the web build
 
-  console.log('Removing usb module from package-lock.json')
+console.log('Removing usb module from package-lock.json')
 
-  const fs = require('fs')
-  const path = require('path')
-  const lock = require('./package-lock.json')
+const lock = require('./package-lock.json')
 
-  delete lock.dependencies.usb
-  delete lock.dependencies['@ledgerhq/hw-transport-node-hid'].requires.usb
+delete lock.dependencies.usb
+delete lock.dependencies['@ledgerhq/hw-transport-node-hid'].requires.usb
 
-  const p = path.join(__dirname, 'package-lock.json')
-
-  console.log('Writing new package-lock.json at ', p)
-
-  fs.writeFileSync(p, JSON.stringify(lock, null, 2), 'utf8')
-}
+require('fs').writeFileSync('./package-lock.json', JSON.stringify(lock, null, 2), 'utf8')
