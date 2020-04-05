d01a014a5797707d73f65299e39fe1e2d63b46fb
Harsh Vakharia <harshjv@gmail.com>
2020-04-06 00:45:47 +0530

    remove node-hid from package-lock.json

--- preinstall.js
+++ preinstall.js
@@ -2,12 +2,15 @@
 // and we don't need it for the web build
 
 if (process.env.NETLIFY) {
-  console.log('Removing usb module from package-lock.json')
+  console.log('Removing usb & node-hid modules from package-lock.json')
 
   const lock = require('./package-lock.json')
 
-  delete lock.dependencies.usb
-  delete lock.dependencies['@ledgerhq/hw-transport-node-hid'].requires.usb
+  delete lock.dependencies['usb']
+  delete lock.dependencies['node-hid']
+  delete lock.dependencies['@ledgerhq/hw-transport-node-hid'].requires['usb']
+  delete lock.dependencies['@ledgerhq/hw-transport-node-hid'].requires['node-hid']
+  delete lock.dependencies['@ledgerhq/hw-transport-node-hid-noevents'].requires['node-hid']
 
   require('fs').writeFileSync('./package-lock.json', JSON.stringify(lock, null, 2), 'utf8')
 }
